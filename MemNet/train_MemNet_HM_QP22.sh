a8821beac935dc0c0dd348d1e78dc29f69d5b825
Yang Wenhan <yangwenhan@pku.edu.cn>
2020-10-13 19:10:43 +0800

    Add files via upload

---
+++ MemNet/train_MemNet_HM_QP22.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=3 python train.py \
+    -t /mnt/hdd/yangwh/benchmark_dataset/bsd_label/ \
+    -tc  /mnt/hdd/yangwh/benchmark_dataset/bsd_22/ \
+    -t1 /mnt/hdd/yangwh/benchmark_dataset/label/ \
+    -tc1 /mnt/hdd/yangwh/benchmark_dataset/22/ \
+    -v /mnt/hdd/yangwh/benchmark_dataset/val_label/ \
+    -vc /mnt/hdd/yangwh/benchmark_dataset/val_22/ \
+    -q 22 \
+    -b 30 \
+    -bs 28 \
+    -md /mnt/hdd/yangwh/compression-artifacts-becnmark-models/models/MemNet/QP22/ \
+    -l 0.0001 \
+    -e 60
+ 
