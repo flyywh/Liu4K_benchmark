31cea85819bbd33f232b02ee9188c647b9ddf272
Yang Wenhan <yangwenhan@pku.edu.cn>
2020-10-13 11:49:28 +0800

    Add files via upload

---
+++ DnCNN_HM/test/test.sh
@@ -0,0 +1,1 @@
+QP=22 CUDA_VISIBLE_DEVICES=5 python test_crop_hm.py -v /mnt/hdd/benchmark_dataset/TEST_SETS/bsd100_label_pad/ -vc /mnt/hdd/benchmark_dataset/TEST_SETS/BSD100_QP22/ -q 22 -md  /mnt/hdd/yangwh/compression-artifacts-benchmark-models/models/DnCNN/QP22/ckpt/1 -od /mnt/hdd/yangwh/compression-artifacts-becnmark/outputs/DnCNN/QP22/BSD100/ -cr False
